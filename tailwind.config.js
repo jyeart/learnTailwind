a5e795fd615b7c62bb2341b6f4da89793d1eafcd
Richard Burd <richardburd@yahoo.com>
2023-03-07 18:50:08 +0000

    TailwindCSS working locally & w/CDN link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+
+  // Richard can't get his VS Code Live server to serve up
+  // any files in a subdirectory, so he's added an index.html
+  // at the root of the project which he can get to work.
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {},
   },
